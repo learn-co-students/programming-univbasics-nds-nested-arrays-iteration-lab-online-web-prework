a190e111cb042a806c4db57f72e2516c35595220
ladeeluc <laura.hatch.lh@gmail.com>
2019-10-13 12:10:22 -0500

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -13,27 +13,39 @@ end
 
 
 
+                                                                        #Psuedocode
+                                                                        # make new array
+                                                                        # put into that new array of strings that takes in first two elements
+                                                                        # needs to increment up for each pair of  first elements of the new array that was joined
 
-# make new array
-# put into that new array of strings that takes in first two elements
-# needs to increment up for each pair of  first elements of the new array that was joined
+                                                                          # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
+                                                                          #
+                                                                          # Build a new Array that contains strings where each pair of foods is
+                                                                          # inserted into this template:
+                                                                          #
+                                                                          # "I love (inner array element 0) and (inner array element 1) on my pizza""
+                                                                          # As such, there should be a new String for each inner array, or pair
 
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
-#binding.pry
 
 
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+    new_array = []
+    scr.map.with_index{|x,i|x > i} 
 end
+
+#psudeocode 
+
+# going to need new array [/]
+# need to pass through each array with in larger array
+# need to grab the larger integer of two element of inner array
+
+        # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
+        # Produce a new Array that contains the larger number of each of the pairs
+        # that are in the inner Arrays
+
+
+
 
 def total_even_pairs(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
