0dc95b50539776b93e5696ca6cb1f26c3de7a5a4
ladeeluc <laura.hatch.lh@gmail.com>
2019-10-21 16:09:45 -0500

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -26,6 +26,17 @@ end
 
 
 def total_even_pairs(src)
+ 
+  if scr.even? == true
+    scr.sum
+  end
+end
+
+
+
+
+
+
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # if both numbers in the pair are even, then add both those numbers to the
   # total
@@ -33,4 +44,4 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
-end
+
