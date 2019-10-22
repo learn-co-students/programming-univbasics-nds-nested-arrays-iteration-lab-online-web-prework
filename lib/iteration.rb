ba6d7d981e95c8dcc99d42cb00f1a89275735245
Laura Hatch <laura.hatch.lh@gmail.com>
2019-10-22 02:47:43 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,8 +1,8 @@
-
+require 'pry'
 
 def join_ingredients(src)
       new_array = []
-
+      
         src.each do |elem1|
         new_array << ("I love #{elem1[0]} and #{elem1[1]} on my pizza" )
     end
@@ -16,32 +16,33 @@ new_array = []
 counter = 0
 
 while counter < src.length
+#binding.pry
   new_array << src[counter].max
-  counter =+ 1
+  counter += 1
 end
 new_array
     
 end
 
 
-
 def total_even_pairs(src)
- new_array = []
-  if scr.even? == true
-  new_array << scr.sum 
-  end
+   total = 0
+   i = 0
+    #counter = 0
+
+      while i < src.length do
+      if (src[i][0] % 2 == 0) && (src[i][1] % 2 == 0)
+      total += (src[i][0]) + (src[i][1])
+    end
+i += 1 
+end
+total
 end
 
+# looked at answer. will review again.
 
 
 
 
-
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
+   
 
