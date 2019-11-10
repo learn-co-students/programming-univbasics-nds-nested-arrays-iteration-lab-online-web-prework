41ab7435f58050422c9216a124b15db07b712c19
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2019-11-10 00:45:39 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,4 +1,6 @@
+
 def join_ingredients(src)
+  
   # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
   #
   # Build a new Array that contains strings where each pair of foods is
@@ -6,21 +8,10 @@ def join_ingredients(src)
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
-  array_1 = [['pepperoni', 'sausage'], ['green olives', 'green peppers'], ['onions', 'pineapple']]
-  
-  array_x = [["I", "love", array_1[0], "and", array_1[1], "on", "my", "pizza"].join(''),
-        ["I", "love", array_1[2], "and", array_1[3], "on", "my", "pizza"].join(''),
-        ["I", "love", array_1[4], "and", array_1[5], "on", "my", "pizza"].join(''),
-]
 
-row_index = 0
-while row_index < array_1.count do
-  element_index = 0
-  while element_index < array_1[row_index].count do
-    puts array_x += 1
-  end
-  row_index += 1
-end 
+inner_array = src[0]inner_element
+string_1 = "I love #{src[0]and [1]} on my pizza."
+
 end
 
 def find_greater_pair(src)
@@ -28,6 +19,7 @@ def find_greater_pair(src)
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
   array_2 = [[-1, -900], [10, 30], [0, 0], [14, 16 * -2.5], [Math.sin(1), 19]]
+  greater_pair =  src[0]inner_element <=> src[1]inner_element
 end
 
 def total_even_pairs(src)
@@ -38,5 +30,9 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
+  
   array_3 = [[86262, 58115], [22558, 97709], [66236, 28056], [25992, 81375], [11248, 56789], [42783, 27353], [70796, 63432], [53234, 63058], [72316, 8675309]]
+  
+  even_pair = src[0]inner_element.even + src[1]inner_element.even 
+  
 end
