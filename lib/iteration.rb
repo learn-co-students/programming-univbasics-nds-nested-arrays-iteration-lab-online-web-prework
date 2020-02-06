c945e909370314ea0210c970e50395d9ee3063ef
Kyle Talmage <ktalmage85@gmail.com>
2020-02-06 23:40:22 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -4,41 +4,15 @@ src = [
   ["onions","pineapple"]
 ]
 
- 
-   
-
-def join_ingredients(src)
-  src_inner = []
-  src_outer = []
+  def join_ingredients(src)
+  result = []
   row_index = 0
-while row_index < src.count do
-  element_index = 0
-  
-  while element_index <
-  src.count do
-    
-  src_inner <<  src[row_index]
+  while row_index < src.count do
+    result << "I love #{src[row_index][0]} and #{src[row_index][1]} on my pizza"
+    row_index += 1
+  end
+  result
 end
-  element_index +=1
-end
-  src_outer << src_inner[row_index]
-  row_index +=1 
-end
-row_index = 0
-while row_index < src_outer.count do
- puts "I love #{src_outer[row_index][element_index]} and #{src_outer[row_index][element_index + 1]} on my pizza"
- end
-    #element_index += 1
-  #row_index += 1
-  #end
-#end
-
-
-#puts join_ingredients(src)
-
-
-
-
 
 # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
   #
@@ -48,15 +22,60 @@ while row_index < src_outer.count do
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
 
+src1 = [
+  [1,2],
+  [3,4],
+  [7,6]
+]
 
-#def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
+def find_greater_pair(src1)
+src2 = []
+
+row_index = 0
+  while row_index < src1.count do
+    
+    if  src1[row_index][0]  > src1[row_index][1]
+    
+    puts src2 << src1[row_index][0]
+  else
+    
+    puts src2 << src1[row_index][1]
+  end
+      row_index +=1
+end
+src2
+end
+
+
+
+#src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
   #end
+src3 = [
+  [2,2],
+  [1,3],
+  [8,4]
+  ]
 
-#def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
+def total_even_pairs(src3)
+row_index = 0
+total = 0
+
+
+  while row_index <src3.count do
+if src3[row_index][0] % 2 == 0 && src3[row_index][1]% 2 == 0
+  puts total += src3[row_index][0] + src3[row_index][1] 
+  end
+    row_index +=1
+  end
+  total
+end
+  
+
+
+
+#srcll be an array of [ [number1, number2], ... [numberN, numberM] ]
   # if both numbers in the pair are even, then add both those numbers to the
   # total
   #
