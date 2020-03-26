aa65ab9d93d9ae7e0e952e4448708fb4345e9964
CleanSolLLC <mamark90@gmail.com>
2020-03-26 17:58:36 -0400

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,25 +1,51 @@
+require 'pry'
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+  new_array = []
+  row_index = 0
+  while row_index < src.length
+    element = 0
+    while element < src[row_index].length
+      new_array << "I love #{src[row_index][element]} and #{src[row_index][element+1]} on my pizza"
+      element +=2
+    end
+    row_index +=1
+  end
+  new_array
+
 end
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+  new_array = []
+  row_index = 0
+  while row_index < src.length
+    element = 0
+    while element < src[row_index].length
+      if src[row_index][element] < src[row_index][element+1]       
+        new_array << src[row_index][element+1]
+      else
+        new_array << src[row_index][element]      
+      end
+      element +=2
+    end
+    row_index +=1
+  end
+  new_array
 end
 
 def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
+  new_array = []
+  row_index = 0
+  while row_index < src.length
+    element = 0
+    while element < src[row_index].length
+      if (src[row_index][element] % 2  &&  src[row_index][element+1] % 2) == 0
+        new_array << src[row_index][element] + src[row_index][element+1]
+      #binding.pry  
+      end
+      element +=2
+    end
+    row_index +=1
+  end
+  new_array.sum
+
 end
